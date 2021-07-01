028fcd854be4e39e7e90e959c2d829be27ec7f5b
Alexandr Toropov <toropovsite@yandex.ru>
2021-07-01 03:34:48 +0300

    Константы статусов

---
+++ Task.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class Task
+ */
+
+class Task
+{
+    const STATUS_NEW = 'new';
+    const STATUS_CANCEL = 'canceled';
+    const STATUS_IN_WORK = 'in_work';
+    const STATUS_DONE = 'done';
+    const STATUS_FAIL = 'fail';
+
+    public function __construct()
+    {
+
+    }
+}

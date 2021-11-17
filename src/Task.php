15991d6b290c77f5d0372e65ece8fb35dea29ad9
Alexandr Toropov <toropovsite@yandex.ru>
2021-11-17 15:49:27 +0300

    Карта статусов возвращает массив строк

--- src/Task.php
+++ src/Task.php
@@ -24,14 +24,19 @@ class Task
     public function getStatusesMap()
     {
         return[
-            self::STATUS_NEW => 1,
-            self::STATUS_CANCEL => 2,
-            self::STATUS_IN_WORK => 3,
-            self::STATUS_DONE => 4,
-            self::STATUS_FAIL => 5,
+            self::STATUS_NEW => 'new',
+            self::STATUS_CANCEL => 'cancel',
+            self::STATUS_IN_WORK => 'inwork',
+            self::STATUS_DONE => 'done',
+            self::STATUS_FAIL => 'fail',
         ];
     }
 
+    /**
+     * Возвращает карту действий 
+     *
+     * @return string[]
+     */
     public function getActionMap()
     {
         return[

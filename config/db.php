fd6b34e87830b1fc4f4a5502c8e82b5886998fa6
Alexandr Toropov <toropovsite@yandex.ru>
2023-12-27 20:48:59 +0300

    Первый шаг на большом пути

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=test',
+    'username' => 'test-user',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];

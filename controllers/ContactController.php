f7b3c7093f31c858204367fbcea27f2ea8cbd30f
Alexandr Toropov <toropovsite@yandex.ru>
2024-02-17 16:01:27 +0300

    Контроллер контактов

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -7,6 +7,7 @@ use yii\web\Controller;
 use yii\web\Response;
 use yii\db\Query;
 use yii\web\NotFoundHttpException;
+use app\models\Contact;
 
 class ContactController extends Controller
 {
@@ -17,5 +18,29 @@ class ContactController extends Controller
         $response->data = $contacts;
         $response->format = Response::FORMAT_JSON;
         return $response;
+    }
+
+    public function actionIndex()
+    {
+        $company = Company::find()->where(1)->joinWith('contact')->one();
+        $contacts = $company->contacts;
+        foreach ($contacts as $contact) {
+            echo $contact->name, $contact->phone, $contact->company->name;
+        }
+    }
+
+    public function actionShow()
+    {
+        $contact = Contact::findOne($id);
+        if (!$contact) {
+            throw new NotFoundHttpException('Контакт с ID ' . $id . ' не найден');
+        }
+        return $this->render('view', ['contact' => $contact]);
+    }
+
+    public function actionFilter($status)
+    {
+        $contacts = Contact::findAll(['status' => $status]);
+        return $this->render('index', ['contact' => $contacts]);
     }
 }
